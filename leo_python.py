7f7dc75031625b7fdb93b5982d2e0974941f99ec
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2021-04-18 02:44:57 +0900

    I added a python file

---
+++ leo_python.py
@@ -0,0 +1,15 @@
+# variable
+my_number = 10
+
+# creating a function
+def my_function(my_input):
+    if my_input == 10:
+        return True
+    else:
+        return "Wrong answer"
+
+
+# declaring your fucntion
+print(my_function(my_number))
+
+
